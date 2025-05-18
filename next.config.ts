a321078703895e6ba18dc628f33ea6163a67dbea
김진영 <jykim@snack24h.com>
2025-05-18 16:20:29 +0900

    fix: staticPageGenerationTimeout 값 조절

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // 캐싱을 방지하기 위한 설정
-  staticPageGenerationTimeout: 0,
+  staticPageGenerationTimeout: 60,
   // 동적 렌더링 강제
   reactStrictMode: true,
   // 에러 페이지 사용자 정의 방지
